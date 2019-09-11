666dae7e35036a78755cd2d8a14a243bb5d740e5
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-09-11 13:26:03 -0500

    Create while_blink.py
    Infinite while loop example

---
+++ Basic/while_blink.py
@@ -0,0 +1,22 @@
+# Chip: ESP32-WROOM-32 (ESP32-D0WDQ6)
+# Microprocessor: 2 Cores low-power Xtensa® 32-bit LX6
+# Clock: 80MHz to 240Mhz
+# Crystal: 40MHz
+# SPÍ flash: 4 MB
+# Operating voltage: 3.0V-3.6V
+# Operating current: 80mA
+
+# Purpose: Puts a pin in high state for a second and in low state for a second while the condition is true
+
+# Notes: If you need to stop a program that's running on the board just connect to the REPL and press Ctrl-c
+
+from machine import Pin
+import time
+
+p0 = Pin(0, Pin.OUT)
+
+while True:
+    p0.on()
+    time.sleep(1)
+    p0.off()
+    time.sleep(1)
