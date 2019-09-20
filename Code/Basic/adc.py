5b1c237ef38cfe27c142423b45f2dbfeb21adb96
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-09-20 08:09:40 -0500

    Create adc.py
    analog to digital converter example

---
+++ Code/Basic/adc.py
@@ -0,0 +1,14 @@
+# Chip: ESP32-WROOM-32 (ESP32-D0WDQ6)
+# Microprocessor: Dual-Core Xtensa® 32-bit LX6
+# Clock: 80MHz to 240Mhz
+# Crystal: 40MHz
+# SPÍ flash: 4 MB
+# Operating voltage: 3.0V-3.6V
+# Operating current: 80mA
+
+# Purpose: Create ADC object on ADC pin and read a value between 0-1024
+
+from machine import ADC
+
+adc = ADC(0)
+adc.read()
