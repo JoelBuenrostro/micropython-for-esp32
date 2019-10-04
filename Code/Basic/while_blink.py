5652b8ed8c3de78a92014400af5ead047fdad542
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-10-04 14:07:07 -0500

    Update while_blink.py
    New device description

--- Code/Basic/while_blink.py
+++ Code/Basic/while_blink.py
@@ -5,18 +5,22 @@
 # SPÍ flash: 4 MB
 # Operating voltage: 3.0V-3.6V
 # Operating current: 80mA
+# Firmware: Micropython v1.11
+# Board: NodeMCU-32s
 
-# Purpose: Puts a pin in high state for a second and in low state for a second while the condition is true
+# Purpose: Puts a pin in high state for a second and in low state for a second while the condition
+# is true
 
-# Notes: If you need to stop a program that's running on the board just connect to the REPL and press Ctrl-c
+# Notes: If you need to stop a program that's running on the board just connect to the REPL and
+# press Ctrl-c
 
 from machine import Pin
 import time
 
-p11 = Pin(11, Pin.OUT)
+led = Pin(2, Pin.OUT)
 
 while True:
-    p0.on()
+    led.on()
     time.sleep(1)
-    p0.off()
+    led.off()
     time.sleep(1)
