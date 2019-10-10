48d710c33856efc430e5c5fc99977c950af19c53
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-10-10 17:57:47 -0500

    Create file_system.py
    listdir example

---
+++ Code/Basic/file_system.py
@@ -0,0 +1,15 @@
+# Chip: ESP32-WROOM-32 (ESP32-D0WDQ6)
+# Microprocessor: Dual-Core Xtensa® 32-bit LX6
+# Clock: 80MHz to 240Mhz
+# Crystal: 40MHz
+# SPÍ flash: 4 MB
+# Operating voltage: 3.0V-3.6V
+# Operating current: 80mA
+# Firmware: Micropython v1.11
+# Board: NodeMCU-32s
+
+# Purpose: Check the uploaded files in the file system
+
+import os
+
+os.listdir()
